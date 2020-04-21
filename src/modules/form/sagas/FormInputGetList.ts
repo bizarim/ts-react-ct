4dcbcdcf92ce2630d8431b28b5f315932216d1c8
met <bizarim82@gmail.com>
2020-04-21 18:49:58 +0900

    Fix: bug fix

--- src/modules/form/sagas/FormInputGetList.ts
+++ src/modules/form/sagas/FormInputGetList.ts
@@ -1,16 +1,11 @@
-import { call, put } from 'redux-saga/effects';
-import { API } from '../../../api';
-import { ServiceType } from '../../../constants';
+import { put } from 'redux-saga/effects';
 import { FormInputGetListReq, formInputGetListRes, formInputGetListErr } from '../actions';
 import data from '../../../assets/input.json';
 import { formModalInitailize } from '../../modal';
 
 export function* getFormInputGetListSaga(action: FormInputGetListReq) {
-
     try {
-        const rs = yield call(API.get({ service: ServiceType.form }), `/input`);
-        // todo
-        // yield put(formInputGetListRes(rs));
+        // todo api 연동
         yield put(formInputGetListRes(data));
         yield(formModalInitailize({ formId: data.formId}));
     } catch (error) {
--- src/modules/form/sagas/FormOutputSummit.ts
+++ src/modules/form/sagas/FormOutputSummit.ts
@@ -1,21 +1,12 @@
-import { put, call } from 'redux-saga/effects';
+import { put } from 'redux-saga/effects';
 import { FormOutputSummitReq, formOutputSummitErr } from '../actions';
-import { eErrorCode, ServiceType } from '../../../constants';
+import { eErrorCode } from '../../../constants';
 import { formModalSubmitCompleted } from '../../modal';
-import { ApiResponse, API } from '../../../api';
 
 export function* getFormOutputSummitSaga(action: FormOutputSummitReq) {
     try {
-        // 답변 제조합 하기
-
-
         // todo api 연동
-        const rs: ApiResponse = yield call(API.post({ service: ServiceType.form }), '/output', action.payload);
-        // if (rs.code === '0')yield put(formOutputSummitRes());
-        // else yield put(formOutputSummitErr({ code: rs.code, msg: rs.msg }));
-
         yield put(formModalSubmitCompleted({}));
-
     } catch (error) {
         // todo 개선
         yield put(formOutputSummitErr({ code: `${eErrorCode.UnknownError}`, msg: 'UnknownError' }));
